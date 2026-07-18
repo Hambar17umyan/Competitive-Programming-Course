499fc1cf84c8b4b962b5ae50e92f9763e2b65870
Sergey Hambardzumyan <hambardzumyan7serg@outlook.com>
2026-07-18 14:19:49 +0400

    DSU Step 1 Problem B Solution 2

--- Competitive Programming Course/DSU_S1_B_2.cpp
+++ Competitive Programming Course/DSU_S1_B_2.cpp
@@ -1,31 +1,37 @@
 #include <iostream>
 using namespace std;
 
-const int N = 2e5 + 5;
+const int N = 3e5 + 5;
 int p[N];
 int sz[N];
+int mn[N], mx[N];
 
 void init(int n) {
 	for (int i = 1; i <= n; i++) {
-		p[i] = i;
+		p[i] = mx[i] = mn[i] = i;
 		sz[i] = 1;
 	}
 }
 
 int get_set(int a) {
 	if (p[a] != a)
+	{
 		p[a] = get_set(p[a]);
+	}
+	
 	return p[a];
 }
 
 void union_sets(int a, int b) {
 	a = get_set(a);
 	b = get_set(b);
-	if (a == b)
+	if (a == b) 
 		return;
 	if (sz[a] > sz[b])
 		swap(a, b);
 	p[a] = b;
+	mx[b] = max(mx[b], mx[a]);
+	mn[b] = min(mn[b], mn[a]);
 	sz[b] += sz[a];
 }
 
@@ -39,9 +45,15 @@ int main() {
 	while (m--) {
 		cin >> op;
 		int a, b;
-		cin >> a >> b;
+		cin >> a;
 		if (op == "union")
+		{
+			cin >> b;
 			union_sets(a, b);
-		else cout << ((get_set(a) == get_set(b)) ? "YES\n" : "NO\n");
+		}
+		else {
+			a = get_set(a);
+			cout << mn[a] << ' ' << mx[a] << ' ' << sz[a] << '\n';
+		}
 	}
 }
